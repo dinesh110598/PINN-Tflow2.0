25cd54bca746ebaaec7d612424a4f43e22c9b965
dinesh110598 <dineshramakrishnan2012@gmail.com>
2020-11-18 21:01:55 +0530

    Passed t, x explicitly to training functions to make differentiations simpler

--- Continuous_Time(Schrodinger)/dummy.py
+++ Continuous_Time(Schrodinger)/dummy.py
@@ -4,11 +4,6 @@ from tensorflow import keras as tfk
 from tensorflow import math as tfm
 import tensorflow as tf
 
-@tf.function
-def tf_diag (v, k=0):
-    y = tf.py_function(np.diag, [v,k], tf.float32)
-    return y
-
 PINN = tfk.Sequential([
     tfk.layers.Input(2),
     tfk.layers.Dense(100, activation=tfk.activations.tanh),
@@ -23,37 +18,38 @@ PINN = tfk.Sequential([
 sgd_opt = tfk.optimizers.SGD()
 
 @tf.function
-def train_colloc (input_var):
+def train_colloc (t, x):
     """Trains the model to obey the given PDE at collocation points
 
     Args:
-        input_var: A tf.Tensor of shape (batch_size,2). Components of second index
-                    denote x and t
+        t: A tf.Tensor of shape (batch_size,)
+        x: A tf.Tensor of shape (batch_size,).
     """
     with tf.GradientTape(True, False) as tape:
-        tape.watch (PINN.trainable_weights)
+        tape.watch(PINN.trainable_weights)
         #Calculate various derivatives of the output
         with tf.GradientTape(True, False) as grtape0:
-            grtape0.watch(input_var)
+            grtape0.watch([t, x])
             with tf.GradientTape(True, False) as grtape1:
-                grtape1.watch(input_var)
+                grtape1.watch([t, x])
                 #Automatic differentiation of complex functions is weird in tensorflow
                 #so we differentiate real and imaginary parts seperately
-                h_real = tfm.real(PINN(input_var))
-                h_imag = tfm.imag(PINN(input_var))
+                h_real = tfm.real(PINN(tf.stack([t, x], -1)))
+                h_imag = tfm.imag(PINN(tf.stack([t, x], -1)))
             #First order derivatives
-            h1_real = grtape1.gradient(h_real, input_var)
-            h1_imag = grtape1.gradient(h_imag, input_var)
+            h_x_real = grtape1.gradient(h_real, x)
+            h_x_imag = grtape1.gradient(h_imag, x)
+            h_t_real = grtape1.gradient(h_real, t)
+            h_t_imag = grtape1.gradient(h_imag, t)
             #h1_real and h1_imag have shape (batch_size,2)
             del grtape1
         #Second order derivatives
-        h2_real = grtape0.jacobian(h1_real, input_var)
-        h2_imag = grtape0.jacobian(h1_real, input_var)
-        #h2_real and h2_imag have shape (batch_size,2,batch_size,2)
+        h_xx_real = grtape0.gradient(h_x_real, x)
+        h_xx_imag = grtape0.gradient(h_x_imag, x)
         del grtape0
         h = tf.complex(h_real, h_imag)
-        h_t = tf.complex(h1_real[:, 0], h1_imag[:, 0])
-        h_xx = tf_diag(tf.complex(h2_real[:, 1, :, 1], h2_imag[:, 1, :, 1]))
+        h_t = tf.complex(h_t_real, h_t_imag)
+        h_xx = tf.complex(h_xx_real, h_xx_imag)
         j = tf.complex(0,1)
 
         MSE = tfm.reduce_euclidean_norm(tfm.abs ((j*h_t) + (0.5*h_xx) + (tfm.conj(h)*h*h)))
@@ -61,62 +57,65 @@ def train_colloc (input_var):
     grads = tape.gradient (MSE, PINN.trainable_weights)
     sgd_opt.apply_gradients (zip(grads,PINN.trainable_weights))
     del tape
+    return MSE
 
 @tf.function
-def train_init (input_var):
+def train_init (t, x):
     """Trains the model to have a fixed initial condition when t=0
 
     Args:
-        input_var : A tf.Tensor of shape (batch_size,2). Components of second index
-                    denote x and t. Value of t should always be 0.
+        t: A tf.Tensor of shape (batch_size,)
+        x: A tf.Tensor of shape (batch_size,).
     """
     def sech(x):
-        return tfm.reciprocal(tfm.cosh(x))
-        
+        return tf.complex (tfm.reciprocal(tfm.cosh(x)), 0)
+
     with tf.GradientTape() as tape:
-        h = PINN (input_var)
-        MSE = tfm.reduce_euclidean_norm ( tfm.abs(h - sech(input_var[:,1])) )
+        h = PINN (tf.stack ([t,x],-1))
+        MSE = tfm.reduce_euclidean_norm (tfm.abs(h - sech(x)))
     grads = tape.gradient(MSE, PINN.trainable_weights)
     sgd_opt.apply_gradients(zip(grads, PINN.trainable_weights))
+    return MSE
 
 @tf.function
-def train_bound (input_times):
+def train_bound (t):
     """Trains the model to equalize values and spatial derivatives at boundaries x=5 
     and x=-5 to enforce periodic boundary condition
 
     Args:
-        input_times : A tf.Tensor of shape (batch_size,). Denotes values of t
+        t : A tf.Tensor of shape (batch_size,).
     """
 
-    input_var1 = tf.stack([input_times, 5*tf.ones(input_times.shape)], axis=-1)
-    input_var2 = tf.stack([input_times,-5*tf.ones(input_times.shape)], axis=-1)
+    x1 = 5*tf.ones (t.shape)
+    x2 = -5*tf.ones (t.shape)
     with tf.GradientTape(True, False) as tape:
         tape.watch (PINN.trainable_weights)
         with tf.GradientTape(True, False) as grtape1:
-            grtape1.watch ([input_var1, input_var2])
+            grtape1.watch ([t, x1, x2])
             #Automatic differentiation of complex functions is weird in tensorflow
             #so we differentiate real and imaginary parts seperately
-            h_real_1 = tfm.real(PINN(input_var1))
-            h_imag_1 = tfm.imag(PINN(input_var1))
-            h_real_2 = tfm.real(PINN(input_var2))
-            h_imag_2 = tfm.imag(PINN(input_var2))
+            h_real_1 = tfm.real(PINN(tf.stack([t, x1], -1)))
+            h_imag_1 = tfm.imag(PINN(tf.stack([t, x1], -1)))
+            h_real_2 = tfm.real(PINN(tf.stack([t, x2], -1)))
+            h_imag_2 = tfm.imag(PINN(tf.stack([t, x2], -1)))
         #First order derivatives
-        h1_real_1 = grtape1.gradient(h_real_1, input_var1)
-        h1_imag_1 = grtape1.gradient(h_imag_1, input_var1)
-        h1_real_2 = grtape1.gradient(h_real_2, input_var2)
-        h1_imag_2 = grtape1.gradient(h_imag_2, input_var2)
+        h_x1_real = grtape1.gradient(h_real_1, x1)
+        h_x1_imag = grtape1.gradient(h_imag_1, x1)
+        h_x2_real = grtape1.gradient(h_real_2, x2)
+        h_x2_imag = grtape1.gradient(h_imag_2, x2)
         #h1_real and h1_imag have shape (batch_size,2)
         del grtape1
         h1 = tf.complex (h_real_1,h_imag_1)
-        h1_x = tf.complex (h1_real_1[:,1], h1_imag_1[:,1])
+        h1_x = tf.complex (h_x1_real, h_x1_imag)
         h2 = tf.complex(h_real_2, h_imag_2)
-        h2_x = tf.complex(h1_real_2[:, 1], h1_imag_2[:, 1])
+        h2_x = tf.complex(h_x2_real, h_x2_imag)
         MSE = tfm.reduce_mean(
                     tfm.pow(tfm.abs(h1-h2),2) + tfm.pow(tfm.abs(h1_x-h2_x),2))
     grads = tape.gradient(MSE, PINN.trainable_weights)
     sgd_opt.apply_gradients(zip(grads, PINN.trainable_weights))
+    return MSE
 
-def training_loop (epochs, batch_size = 25, Ni = 50, Nb = 50, Nf = 20000):
+def training_loop (epochs, batch_size = 10, Ni = 50, Nb = 50, Nf = 20000):
     """Randomly generates points on spacetime that PINN needs to train on
     runs the training loop for PINN.
 
@@ -131,20 +130,22 @@ def training_loop (epochs, batch_size = 25, Ni = 50, Nb = 50, Nf = 20000):
     Nb_b = Nb // batch_size
     Nf_b = Nf // batch_size
     data_len = Ni_b + Nb_b + Nf_b
-    pos = np.random.choice (data_len, 4, False)
-    init_pos = pos[:2]
-    bound_pos = pos[2:]
+    pos = np.random.choice (data_len, Ni_b + Nb_b, False)
+    init_pos = pos[:Ni_b]
+    bound_pos = pos[Ni_b:]
 
     for _ in range (epochs):
         for step in range (data_len):
             if step in init_pos:
-                in_times = tf.zeros (batch_size)
-                in_pos = tf.random.uniform ((batch_size,), -5, 5)
-                train_init (tf.stack ([in_times, in_pos], -1))
+                t = tf.zeros (batch_size)
+                x = tf.random.uniform ((batch_size,), -5, 5)
+                loss = train_init(t, x)
             elif step in bound_pos:
-                in_times = tf.random.uniform ((batch_size,), 0, math.pi/2)
-                train_bound (in_times)
+                t = tf.random.uniform ((batch_size,), 0, math.pi/2)
+                loss = train_bound (t)
             else:
-                in_pos = tf.random.uniform((batch_size,), -5, 5)
-                in_times = tf.random.uniform((batch_size,), 0, math.pi/2)
-                train_colloc (tf.stack([in_times, in_pos], -1))
+                t = tf.random.uniform((batch_size,), 0, math.pi/2)
+                x = tf.random.uniform((batch_size,), -5, 5)
+                loss = train_colloc (t, x)
+            if step % 50 == 0:
+                print ()
